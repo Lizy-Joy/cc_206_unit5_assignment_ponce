20596471eee91177e814c2c6514dab01bbd6846d
Lizy-Joy <lizyjoy.ponce@wvsu.edu.ph>
2024-10-24 22:58:59 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,274 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  int sum = 0;
+  int difference = 0;
+  int product = 0;
+  int quotient = 0;
+  int firstAddNum = 0;
+  int secondAddNum = 0;
+
+  // TextEditingControllers to manage input
+  TextEditingController add1Controller = TextEditingController();
+  TextEditingController add2Controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Unit 5 Calculator"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // Addition Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: add1Controller,
+                  decoration: const InputDecoration(labelText: "First Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      firstAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              const Text(" + "),
+              Expanded(
+                child: TextField(
+                  controller: add2Controller,
+                  decoration: const InputDecoration(labelText: "Second Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      secondAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              Text(' = $sum'),
+              // IconButton for addition
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    sum = firstAddNum + secondAddNum;
+                  });
+                },
+              ),
+              // Clear Button
+              // TextButton(
+              //   onPressed: () {
+              //     setState(() {
+              //       // Clear the TextFields and reset sums
+              //       add1Controller.clear();
+              //       add2Controller.clear();
+              //       firstAddNum = 0;
+              //       secondAddNum = 0;
+              //       sum = 0;
+              //     });
+              //   },
+              //   child: const Text("Clear"),
+              // ),
+            ],
+          ),
+
+          // Subtract Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: add1Controller,
+                  decoration: const InputDecoration(labelText: "First Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      firstAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              const Text(" - "),
+              Expanded(
+                child: TextField(
+                  controller: add2Controller,
+                  decoration: const InputDecoration(labelText: "Second Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      secondAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              Text(' = $difference'),
+              // IconButton for subtraction
+              IconButton(
+                icon: const Icon(Icons.remove),
+                onPressed: () {
+                  setState(() {
+                    difference = firstAddNum - secondAddNum;
+                  });
+                },
+              ),
+              // Clear Button
+              // TextButton(
+              //   onPressed: () {
+              //     setState(() {
+              //       // Clear the TextFields and reset sums
+              //       add1Controller.clear();
+              //       add2Controller.clear();
+              //       firstAddNum = 0;
+              //       secondAddNum = 0;
+              //       difference = 0;
+              //     });
+              //   },
+              //   child: const Text("Clear"),
+              // ),
+            ],
+          ),
+
+          // Multiplication Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: add1Controller,
+                  decoration: const InputDecoration(labelText: "First Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      firstAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              const Text(" * "),
+              Expanded(
+                child: TextField(
+                  controller: add2Controller,
+                  decoration: const InputDecoration(labelText: "Second Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      secondAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              Text(' = $product'),
+              // IconButton for multiplication
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    product = firstAddNum * secondAddNum;
+                  });
+                },
+              ),
+              // Clear Button
+              // TextButton(
+              //   onPressed: () {
+              //     setState(() {
+              //       // Clear the TextFields and reset sums
+              //       add1Controller.clear();
+              //       add2Controller.clear();
+              //       firstAddNum = 0;
+              //       secondAddNum = 0;
+              //       product = 0;
+              //     });
+              //   },
+              //   child: const Text("Clear"),
+              // ),
+            ],
+          ),
+
+          // Division Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: add1Controller,
+                  decoration: const InputDecoration(labelText: "First Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      firstAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              const Text(" / "),
+              Expanded(
+                child: TextField(
+                  controller: add2Controller,
+                  decoration: const InputDecoration(labelText: "Second Number"),
+                  onChanged: (value) {
+                    setState(() {
+                      secondAddNum = int.tryParse(value) ?? 0;
+                    });
+                  },
+                ),
+              ),
+              Text(' = $quotient'),
+              // IconButton for division
+              IconButton(
+                icon: const Icon(Icons.horizontal_rule),
+                onPressed: () {
+                  setState(() {
+                    quotient = secondAddNum != 0 ? firstAddNum ~/ secondAddNum : 0;
+                  });
+                },
+              ),
+              
+            ],
+          ),
+          // Clear Button
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    // Clear the TextFields and reset sums
+                    add1Controller.clear();
+                    add2Controller.clear();
+                    firstAddNum = 0;
+                    secondAddNum = 0;
+                    sum = 0;
+                    difference = 0;
+                    product = 0;
+                    quotient = 0;
+                  });
+                },
+                child: const Text("Clear"),
+              ),
+        ],
+      ),
+    );
+  }
+}
